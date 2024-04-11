15b124fef24601e0826681429dee7a064b5b9cba
avish <avishusa45@gmail.com>
2024-04-11 18:37:40 -0500

    GET request for all data

--- index.js
+++ index.js
@@ -36,5 +36,19 @@ app.post('/api/products',async (req,res)=>{
             })
 
         }
+});
+
+app.get('/api/products',async (req,res)=>{
+
+    try{
+        const products = await Product.find({});
+        res.status(200).json(products)
+    }
+        catch(error){
+            res.status(500).json({
+                message:error.message
+            })
+
+        }
 })
 
