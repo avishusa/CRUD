9fece5159e47cfe0ffa59ddb55eb529636f6b4f3
avish <avishusa45@gmail.com>
2024-04-11 18:27:06 -0500

    POST API

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express= require('express');
 const app=express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const Product = require('./models/product.model');
 
 dotenv.config();
 
+app.use(express.json())
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("You have sucessfully connected to the database !!")
 }).catch(()=>{
@@ -20,5 +22,19 @@ app.listen(port,()=>{
 app.get('/',(req,res)=>{
 
     res.send("The server is up and running perfectly !!")
+});
+
+app.post('/api/products',async (req,res)=>{
+
+    try{
+        const product = await Product.create(req.body);
+        res.status(200).json(product)
+    }
+        catch(error){
+            res.status(500).json({
+                message:error.message
+            })
+
+        }
 })
 
